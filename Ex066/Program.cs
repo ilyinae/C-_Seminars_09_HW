c045da8339c52b8ab9ad8cf2c19cfcc71bbc27d8
Anton Ilyin <ilyin.a.e@gmail.com>
2022-10-15 11:24:26 +0300

    Added Ex066

---
+++ Ex066/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int f(int a, int b)
+{
+    if (a == b)
+        return b;
+    return f(a, b - 1) + b;
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(Math.Min(m, n), Math.Max(m,n)));
